a103a6d07a7211a3ba2d2982013366ec5c15a842
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-11 18:25:00 +0000

    upload the files

---
+++ app.py
@@ -0,0 +1,22 @@
+from vllm import SamplingParams
+from vllm import LLM
+
+class InferlessPythonModel:
+    def initialize(self):
+        model_id = "TheBloke/CodeLlama-34B-Python-GPTQ"
+        self.llm = LLM(model=model_id,quantization="gptq")
+    
+    def infer(self, inputs):
+        prompts = inputs["prompt"]
+        sampling_params = SamplingParams(
+            temperature=1.0,
+            top_p=1,
+            max_tokens=512
+        )
+        result = self.llm.generate(prompts, sampling_params)
+        result_output = [output.outputs[0].text for output in result]
+
+        return {"result": result_output[0]}
+
+    def finalize(self):
+        self.llm = None
